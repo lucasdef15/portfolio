71a9cacfd495d3acbcae80b3fca04951f8b1c900
lucas <lucasff15@hotmail.com>
2023-06-01 13:59:32 -0300

    feat: 'added new project'

--- src/data/ProjectsData.js
+++ src/data/ProjectsData.js
@@ -1,8 +1,6 @@
 import rickandmortyimg from '../assets/projects/rickandmorty.png';
 import todolistimg from '../assets/projects/todolist.png';
 
-import cover from '../assets/projects/cover.jpg';
-
 import githubpage1 from '../assets/projects/githubimgs/page1.png';
 import githubpage2 from '../assets/projects/githubimgs/page2.png';
 import githubpage3 from '../assets/projects/githubimgs/page3.png';
@@ -19,6 +17,12 @@ import ecomerce2 from '../assets/projects/ecomerce/page2.png';
 import ecomerce3 from '../assets/projects/ecomerce/page3.png';
 import ecomerce4 from '../assets/projects/ecomerce/page4.png';
 import ecomerce5 from '../assets/projects/ecomerce/page5.png';
+
+import newsletter1 from '../assets/projects/newsletter/page1.png';
+import newsletter2 from '../assets/projects/newsletter/page2.png';
+import newsletter3 from '../assets/projects/newsletter/page3.png';
+import newsletter4 from '../assets/projects/newsletter/page4.png';
+import newsletter5 from '../assets/projects/newsletter/page5.png';
 
 export const projectsData = [
   {
@@ -49,6 +53,22 @@ export const projectsData = [
   },
   {
     id: '2',
+    title: 'Newsletter Sign-up',
+    description:
+      'This is a project developed by Frontend Mentor, which I have implemented using React Router for efficient route configuration. To enhance the styling and design, I utilized styled components. The entire application was built using React.js, while form validation was accomplished using React Hook Form and Yup. Additionally, the project is fully responsive.',
+    techStack: 'React.js, Styled-components, React-Hook-Form, Yup',
+    livePreviewLink: 'https://newsletter-sign-up-sigma.vercel.app/',
+    codeLink: 'https://github.com/lucasdef15/newsletter-sign-up',
+    coverImage: [
+      newsletter1,
+      newsletter2,
+      newsletter3,
+      newsletter4,
+      newsletter5,
+    ],
+  },
+  {
+    id: '3',
     title: 'E-commerce',
     description:
       'A sleek and modern e-commerce platform designed for sewing enthusiasts of all levels. Features include a shopping cart and individual product pages with detailed information, high-quality images, and customer reviews.',
@@ -58,7 +78,7 @@ export const projectsData = [
     coverImage: [ecomerce1, ecomerce2, ecomerce3, ecomerce4, ecomerce5],
   },
   {
-    id: '3',
+    id: '4',
     title: 'Todo-list',
     description:
       'Keep track of your tasks and stay organized with this intuitive todo-list app. Add, edit delete items and mark them as completed. With responsive user interface, this app is perfect for personal use.',
@@ -68,7 +88,7 @@ export const projectsData = [
     coverImage: [todolistimg],
   },
   {
-    id: '4',
+    id: '5',
     title: 'Rick and Morty Api',
     description:
       'Explore the world of Rick and Morty with this API that provides access to information about characters. With a simple and user-friendly interface, this API is perfect for developers and fans.',
@@ -77,14 +97,14 @@ export const projectsData = [
     codeLink: 'https://github.com/lucasdef15/Rick_and_Morty_API_Project',
     coverImage: [rickandmortyimg],
   },
-  {
-    id: '5',
-    title: 'Upcoming',
-    description:
-      'This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content',
-    techStack: '',
-    livePreviewLink: '#',
-    codeLink: '#',
-    coverImage: [cover],
-  },
+  // {
+  //   id: '5',
+  //   title: 'Upcoming',
+  //   description:
+  //     'This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content',
+  //   techStack: '',
+  //   livePreviewLink: '#',
+  //   codeLink: '#',
+  //   coverImage: [cover],
+  // },
 ];
