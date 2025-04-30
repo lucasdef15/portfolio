73476a63bfc1b9518c986b0e35227c2cfd0a8304
lucasdef15 <lucasff15@hotmail.com>
2025-04-30 16:49:08 -0300

    modified:   package-lock.json 	modified:   package.json 	modified:   src/assets/lucas_faria.jpg 	new file:   src/assets/lucas_faria1.jpg 	modified:   src/components/Hero.jsx 	modified:   src/styles/Home.css

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
-import lucas_img from '../assets/lucas_faria.webp';
+import { useRef } from 'react';
+import lucas_img from '../assets/lucas_faria1.jpg';
 import { motion } from 'framer-motion';
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
 
 const heroImgVariants = {
   initial: {
@@ -17,15 +19,64 @@ const heroImgVariants = {
   },
 };
 
+const waveAnimation = {
+  animate: {
+    rotate: [0, 20, -10, 20, -5, 0],
+    transition: {
+      duration: 1.5,
+      repeat: Infinity,
+      ease: 'easeInOut',
+    },
+  },
+};
+
 export default function Hero() {
+  useGSAP(() => {
+    gsap.fromTo(
+      '.hero__text p',
+      { y: -50, opacity: 0 },
+      {
+        y: 0,
+        opacity: 1,
+        stagger: 0.25,
+        duration: 1,
+        ease: 'power2.inOut',
+      }
+    );
+  }, []);
   return (
     <div className="hero" id="hero">
       <div className="hero__text">
-        <p>Hello there! 👋</p>
         <p>
-          I'm <span>Lucas!</span>
+          Ei, tudo bem?{' '}
+          <motion.span
+            style={{
+              display: 'inline-block',
+              originX: 0.7,
+              originY: 0.7,
+              fontFamily:
+                'system-ui, Apple Color Emoji, Segoe UI Emoji, Noto Color Emoji',
+            }}
+            {...waveAnimation}
+          >
+            👋
+          </motion.span>
         </p>
-        <p>A web developer.</p>
+        <div>
+          <p>
+            Eu sou <span className="name">Lucas</span>,
+          </p>
+          <p
+            style={{
+              fontSize: '25px',
+              lineHeight: '30px',
+              marginBlock: '1rem',
+            }}
+          >
+            criando soluções digitais inovadoras e interfaces que conectam
+            ideias ao impacto.
+          </p>
+        </div>
       </div>
       <motion.div
         className="circle"
